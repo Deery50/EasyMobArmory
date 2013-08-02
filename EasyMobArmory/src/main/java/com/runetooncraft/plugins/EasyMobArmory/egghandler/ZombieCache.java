65d3fe516f7e5de49e3e3d8ff3cc2c4ef2bdc904
Deery50 <marcus@marcusman.com>
2013-08-01 21:26:53 -0400

    Cache fix

--- EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/egghandler/ZombieCache.java
+++ EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/egghandler/ZombieCache.java
@@ -4,9 +4,9 @@ import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
 
 public class ZombieCache {
-	ItemStack[] Equip;
-	ItemStack handitem;
-	Boolean isbaby;
+	ItemStack[] Equip = null;
+	ItemStack handitem = null;
+	Boolean isbaby = null;
 	ZombieCache(ItemStack[] equip, ItemStack handitem, Boolean isbaby) {
 		this.Equip.equals(equip);
 		this.handitem.equals(handitem);
