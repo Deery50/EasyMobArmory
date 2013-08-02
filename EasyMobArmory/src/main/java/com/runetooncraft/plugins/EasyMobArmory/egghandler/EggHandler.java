e1e5f95176a3ccaa5d1bf5ea029769012d05ff93
Deery50 <marcus@marcusman.com>
2013-08-02 13:53:51 -0400

    changed horse entity type

--- EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/egghandler/EggHandler.java
+++ EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/egghandler/EggHandler.java
@@ -343,7 +343,7 @@ public class EggHandler {
 		c.setAgeLock(set.agelock);
 		return c;
 	}else if(etype.equals(EntityType.HORSE) && !HorseCache.containsKey(id)) {
-		CommonEntity entity = CommonEntity.create(etype);
+		CommonEntity entity = CommonEntity.create(EntityType.HORSE);
 		String entityLoc = "Eggs.id." + id + ".";
 		Boolean isbaby = eggs.getBoolean(entityLoc + "isbaby");
 		Boolean agelock = eggs.getBoolean(entityLoc + "agelock");
