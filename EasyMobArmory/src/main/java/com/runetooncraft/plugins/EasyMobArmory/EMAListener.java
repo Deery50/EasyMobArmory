4c478e567543de2b3aee29d303de9d48ebb61384
Deery50 <marcus@marcusman.com>
2013-08-03 17:11:34 -0400

    third attempt to fix NullPointerException

--- EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/EMAListener.java
+++ EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/EMAListener.java
@@ -356,7 +356,7 @@ public class EMAListener implements Listener {
 			ItemStack[] InvItems = i.getContents();
 			Inventory NewInv = Bukkit.createInventory(event.getPlayer(), 54, "Spawnerinv");
 			for(ItemStack is : InvItems) {
-				if(is.getType().equals(Material.MONSTER_EGG) && is.hasItemMeta()) {
+				if(is.getTypeId() != 0 && is.getType().equals(Material.MONSTER_EGG) && is.hasItemMeta()) {
 					if(is.getItemMeta().hasDisplayName() && is.getItemMeta().getDisplayName().contains(":")) {
 					NewInv.addItem(is);
 					}
