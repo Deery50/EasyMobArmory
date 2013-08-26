95f4fb122143f7a87c0bc3714fe5cb473a32efcb
Deery50 <marcus@marcusman.com>
2013-08-26 17:15:36 -0400

    fixed problem with usage of certain commands.

--- EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/Commandlistener.java
+++ EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/Commandlistener.java
@@ -23,7 +23,7 @@ public class Commandlistener implements CommandExecutor {
 					if(args[0].equalsIgnoreCase("enable")) {EMAListener.Armoryenabled.put(p, true); Messenger.playermessage("EasyMobArmory enabled", p);}
 					else if(args[0].equalsIgnoreCase("disable")) {EMAListener.Armoryenabled.put(p, false); Messenger.playermessage("EasyMobArmory disabled", p);}
 					else{Usage(p);}
-				}else if(p.hasPermission("ema.spawners")) {
+				}if(p.hasPermission("ema.spawners")) {
 					if(args[0].equalsIgnoreCase("stopspawner")) {SpawnerHandler.CancelSpawnTimer(p);}
 					else if(args[0].equalsIgnoreCase("startspawner")) {SpawnerHandler.StartSpawnTimer(p);}
 					else{Usage(p);}
