a7994fea13b6983df5725becacc879e17cbef918
Deery50 <marcus@marcusman.com>
2013-08-26 18:19:49 -0400

    Added some more temporary fixes

--- EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/Commandlistener.java
+++ EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/Commandlistener.java
@@ -19,12 +19,13 @@ public class Commandlistener implements CommandExecutor {
 			if(args.length == 0) {
 				Usage(p);
 			}else if(args.length == 1) {
-				if(p.hasPermission("ema.use") && args[0].equalsIgnoreCase("enable") || p.hasPermission("ema.use") && args[0].equalsIgnoreCase("disable")) {
+				String commandstring = args[0].toLowerCase();
+				if(p.hasPermission("ema.use") && commandstring.equals("enable") || p.hasPermission("ema.use") && commandstring.equals("disable")) {
 					if(args[0].equalsIgnoreCase("enable")) {EMAListener.Armoryenabled.put(p, true); Messenger.playermessage("EasyMobArmory enabled", p);}
 					else if(args[0].equalsIgnoreCase("disable")) {EMAListener.Armoryenabled.put(p, false); Messenger.playermessage("EasyMobArmory disabled", p);}
 					else{Usage(p);}
 				}
-				if(p.hasPermission("ema.spawners")) {
+				if(p.hasPermission("ema.spawners") && commandstring.equals("stopspawner") || p.hasPermission("ema.spawners") && commandstring.equals("startspawner")) {
 					if(args[0].equalsIgnoreCase("stopspawner")) {SpawnerHandler.CancelSpawnTimer(p);}
 					else if(args[0].equalsIgnoreCase("startspawner")) {SpawnerHandler.StartSpawnTimer(p);}
 					else{Usage(p);}
