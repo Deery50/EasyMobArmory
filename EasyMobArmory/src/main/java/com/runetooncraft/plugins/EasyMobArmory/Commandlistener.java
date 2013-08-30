95b11e54cb2e5d91739688d25b2fb80d0f84032f
Deery50 <marcus@marcusman.com>
2013-08-30 00:01:17 -0400

    Removed annoying usage message. You can now access it with /ema help

--- EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/Commandlistener.java
+++ EasyMobArmory/src/main/java/com/runetooncraft/plugins/EasyMobArmory/Commandlistener.java
@@ -20,15 +20,14 @@ public class Commandlistener implements CommandExecutor {
 				Usage(p);
 			}else if(args.length == 1) {
 				String commandstring = args[0].toLowerCase();
-				if(p.hasPermission("ema.use") || commandstring.equals("enable") && p.hasPermission("ema.use") || commandstring.equals("disable")) {
+				if(p.hasPermission("ema.use")) {
 					if(args[0].equalsIgnoreCase("enable")) {EMAListener.Armoryenabled.put(p, true); Messenger.playermessage("EasyMobArmory enabled", p);}
 					else if(args[0].equalsIgnoreCase("disable")) {EMAListener.Armoryenabled.put(p, false); Messenger.playermessage("EasyMobArmory disabled", p);}
-					else{Usage(p);}
+					else if(args[0].equalsIgnoreCase("help")) {Usage(p);}
 				}
-				if(p.hasPermission("ema.spawners") || commandstring.equals("stopspawner") && p.hasPermission("ema.spawners") || commandstring.equals("startspawner")) {
+				if(p.hasPermission("ema.spawners")) {
 					if(args[0].equalsIgnoreCase("stopspawner")) {SpawnerHandler.CancelSpawnTimer(p);}
 					else if(args[0].equalsIgnoreCase("startspawner")) {SpawnerHandler.StartSpawnTimer(p);}
-					else{Usage(p);}
 				}else{
 					Messenger.NoPermissionCommand(p);
 				}
@@ -37,7 +36,6 @@ public class Commandlistener implements CommandExecutor {
 					if(args[0].equalsIgnoreCase("setspawntick")) {SpawnerHandler.SetSpawnTick(p,args[1]);}
 					else if(args[0].equalsIgnoreCase("setdetectionradius")) {SpawnerHandler.setDetectionRadius(p,args[1]);}
 					else if(args[0].equalsIgnoreCase("setradius")) {SpawnerHandler.setMonsterSpawnRadius(p,args[1]);}
-					else{Usage(p);}
 				}else{
 					Messenger.NoPermissionCommand(p);
 				}
